7924f494ba277d7ec8aa1e9076084b7d872df4bd
MikeyBeez <mbonsign@gmail.com>
2024-06-02 12:20:55 -0500

    new config values

--- src/config.py
+++ src/config.py
@@ -3,6 +3,9 @@ latitude = 36.3760 # Replace with your latitude
 longitude = -92.5892 # Replace with your longitude
 noaa_weather_token = "ctVbFFiKVXrINLkJSAEsnCAnhLoyoMqO"
 embedding_model_name = "all-minilm"  # Or your preferred embedding model
+# config.py
+SIMILARITY_THRESHOLD = 0.8
+TOP_N_RESULTS = 5
 WELCOME_MESSAGE = """
 *****************************
 **  Welcome to the Chatbot! **
