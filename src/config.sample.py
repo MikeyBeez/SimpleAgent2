52bb01083dbedb0353044c66c2c002bf8e513623
MikeyBeez <mbonsign@gmail.com>
2024-06-02 21:31:30 -0500

    moved config.py to config.sample.py

---
+++ src/config.sample.py
@@ -0,0 +1,15 @@
+#Copy this to config.py and add your info
+zip_code = "72619"
+latitude = 36.3760 # Replace with your latitude
+longitude = -92.5892 # Replace with your longitude
+noaa_weather_token = "ctVbFFiKVXrINLkJSAEsnCAnhLoyoMqO"
+embedding_model_name = "all-minilm"  # Or your preferred embedding model
+# config.py
+SIMILARITY_THRESHOLD = 0.8
+TOP_N_RESULTS = 5
+WELCOME_MESSAGE = """
+*****************************
+**  Welcome to the Chatbot! **
+*****************************
+"""
+
